2decc986e36733475a1f4d571142660ab034d621
Jeremy Dothager <jeremydothager@gmail.com>
2017-03-06 19:45:20 -0600

    added dotnet command line tools and langDict functions

--- src/HelloMVC/Controllers/HelloController.cs
+++ src/HelloMVC/Controllers/HelloController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -7,9 +8,47 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select name='lang'>" +
+                    "<option value='french'>French</option>" +
+                    "<option value='spanish'>Spanish</option>" +
+                    "<option value='russian'>Russian</option>" +
+                    "<option value='chinese'>Chinese</option>" +
+                    "<option value='german'>German</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World", string lang = "chinese")
+        {
+            Dictionary<string, string> langDict = new Dictionary<string, string>
+            {
+                { "english", "Howdy!" },
+                { "chinese", "Ni Hao" },
+                { "french", "Bonjour" },
+                { "german", "Hallo" },
+                { "spanish", "Hola" }
+            };
+
+            string message = string.Format("<h1>{0} {1}!</h1>", langDict[lang], name);
+            return Content(message, "text/html");
+        }
+
+        // Handle requests to /Hello/Chris (URL segement)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
         }
 
         public IActionResult Goodbye()
